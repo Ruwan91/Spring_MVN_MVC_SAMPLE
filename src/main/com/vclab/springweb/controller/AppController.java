81e03c0fd1db96bc72af9cdbd05b92e004315e7a
Ruwan91 <ruwan97@gmail.com>
2018-10-02 16:59:20 +0530

    changed pom again

--- src/main/com/vclab/springweb/controller/AppController.java
+++ src/main/com/vclab/springweb/controller/AppController.java
@@ -1,22 +1,16 @@
 package com.vclab.springweb.controller;
 
-import com.google.gson.Gson;
 import com.vclab.springweb.model.Customer;
 import com.vclab.springweb.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/test")
