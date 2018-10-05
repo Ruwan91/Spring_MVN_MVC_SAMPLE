9a14bdf9f11a7d8f3a48785d3340f4004a105fb9
Ruwan91 <ruwan97@gmail.com>
2018-10-05 11:42:59 +0530

    Edit new CustomerDAOImplTest class and connect with the H2 DB testing findAll customers, delete customer, update customer and add new customer test methods

--- src/test/com/vclab/springweb/dao/CustomerDAOImplTest.java
+++ src/test/com/vclab/springweb/dao/CustomerDAOImplTest.java
@@ -1,15 +1,16 @@
 package com.vclab.springweb.dao;
 
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.springframework.stereotype.Repository;
+import org.junit.runners.MethodSorters;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
-@Repository
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CustomerDAOImplTest {
     private final Integer CID = 1;
     private final String NAME = "Ruwan Peiris";
@@ -17,19 +18,22 @@ public class CustomerDAOImplTest {
     private final String NIC = "123456789V";
     private final Boolean STATUS = true;
 
+    private final Integer CID1 = 2;
+    private final String NAME1 = "Kasun Perera";
+    private final String ADDRESS1 = "Panadura";
+    private final String NIC1 = "789456123V";
+
     @Test
-    public void checkH2DBAndsetupTest(){
+    public void aCheckH2DBAndsetupTest() {
         String sql1 = "DROP TABLE IF EXISTS customer";
         String sql2 = "CREATE TABLE customer (cid INT PRIMARY KEY, name VARCHAR(200),address VARCHAR(200),nic VARCHAR(11),active TINYINT(4))";
         try {
             Class.forName("org.h2.Driver");
-//            System.out.println("After Driver load");
             Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "ruwan", "vclab");
-//            System.out.println("After the connection");
-            conn.createStatement().executeUpdate(sql1);
-            conn.createStatement().executeUpdate(sql2);
+            int i = conn.createStatement().executeUpdate(sql1);
+            int i1 = conn.createStatement().executeUpdate(sql2);
 
-//            Assert.assertTrue( > 0);
+            Assert.assertTrue(i == 0 && i1 == 0);
             conn.close();
         } catch (SQLException | ClassNotFoundException se) {
             Logger.getLogger(String.valueOf(se));
@@ -37,14 +41,16 @@ public class CustomerDAOImplTest {
     }
 
     @Test
-    public void saveCustomer() {
+    public void bestSaveCustomerTestCase() {
         String sql = "INSERT INTO customer VALUES(" + CID + ",'" + NAME + "','" + ADDRESS + "','" + NIC + "'," + STATUS + ")";
+        String sql1 = "INSERT INTO customer VALUES(" + CID1 + ",'" + NAME1 + "','" + ADDRESS1 + "','" + NIC1 + "'," + STATUS + ")";
         try {
             Class.forName("org.h2.Driver");
-//            System.out.println("After Driver load");
             Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "ruwan", "vclab");
-//            System.out.println("After the connection");
-            Assert.assertTrue(conn.createStatement().executeUpdate(sql) > 0);
+            int i = conn.createStatement().executeUpdate(sql);
+            int i1 = conn.createStatement().executeUpdate(sql1);
+            Assert.assertTrue(i > 0);
+            Assert.assertTrue(i1 > 0);
             conn.close();
         } catch (SQLException | ClassNotFoundException se) {
             Logger.getLogger(String.valueOf(se));
@@ -53,23 +59,61 @@ public class CustomerDAOImplTest {
     }
 
     @Test
-    public void getAllCustomersTest() {
-
-
+    public void callGetAllCustomersTestCase() {
+        String sql = "SELECT * FROM customer";
+        try {
+            Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test","ruwan", "vclab");
+            int i = conn.createStatement().executeUpdate(sql);
+            Assert.assertTrue(i > 0 );
+            conn.close();
+        }catch (SQLException| ClassNotFoundException e){
+            Logger.getLogger(String.valueOf(e));
+        }
     }
 
     @Test
-    public void deleteCustomer() {
-
+    public void deleteCustomerTestCase() {
+        String sql="DELETE FROM customer where cid="+ CID1;
+        try {
+            Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "ruwan", "vclab");
+            int i = conn.createStatement().executeUpdate(sql);
+            Assert.assertTrue(i > 0);
+            conn.close();
+        }catch (ClassNotFoundException | SQLException e){
+            Logger.getLogger(String.valueOf(e));
+        }
     }
 
     @Test
-    public void findCustomerById() {
-
+    public void findCustomerByIdTestCase() {
+        String sql="SELCT * FROM customer where cid="+ CID;
+        try {
+            Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "ruwan", "vclab");
+            int i = conn.createStatement().executeUpdate(sql);
+            Assert.assertTrue(i > 0 );
+            conn.close();
+        }catch (ClassNotFoundException | SQLException e){
+            Logger.getLogger(String.valueOf(e));
+        }
     }
 
     @Test
-    public void updateCustomer() {
-
+    public void updateCustomerTestCase() {
+        String nic = "777777777V";
+        String sql="UPDATE customer SET nic='"+nic+"' WHERE cid="+CID;
+        try {
+            Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "ruwan", "vclab");
+            int i = conn.createStatement().executeUpdate(sql);
+            Assert.assertTrue(i > 0);
+            Assert.assertFalse(i < 1 );
+            Assert.assertEquals(1,i);
+            conn.close();
+        }catch (ClassNotFoundException | SQLException e){
+            Logger.getLogger(String.valueOf(e));
+        }
     }
 }
--- src/test/com/vclab/springweb/service/CustomerServiceTest.java
+++ src/test/com/vclab/springweb/service/CustomerServiceTest.java
@@ -1,21 +1,17 @@
 package com.vclab.springweb.service;
 
+import com.vclab.springweb.dao.CustomerDAOImplTest;
 import org.junit.Test;
 
 
 public class CustomerServiceTest {
 
-    private final Integer CID = 1;
-    private final String NAME = "Ruwan Peiris";
-    private final String ADDRESS = "Moratuwa";
-    private final String NIC = "123456789V";
-    private final Boolean STATUS = true;
-
-
     @Test
     public void saveCustomerTestCase(){
-
+        System.out.println("On testing..");
     }
+
+
 
 
 }
